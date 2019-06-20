7855257026c1a1812f78b7394337ce63011f7b9a
Alexey <redspirit2@ya.ru>
2019-06-21 00:23:18 +0300

    leaders screen

--- scripts/Leaders.gd
+++ scripts/Leaders.gd
@@ -2,7 +2,8 @@ extends Node2D
 
 
 func _ready():
-	$HTTPRequest.getLeaders()
+	$ListNode/ItemNode.visible = false
+	$HTTPRequest.request("http://redspirit.ru:5500/api/leaders")
 	
 
 func _process(delta):
@@ -10,7 +11,24 @@ func _process(delta):
 		get_tree().change_scene("res://scenes/Menu.tscn")
 
 
-func _on_HTTPRequest_onLeaders(items):
+func makeList(items):
+	var i = 0
 	for item in items:
-		print("ITEM ", item)
+		i += 1
+		var node = $ListNode/ItemNode.duplicate(0)
+		node.visible = true
+		node.position.y = (i - 1) * 48 
+		node.get_node("num").text = str(i)
+		node.get_node("name").text = item.name
+		node.get_node("score").text = str(item.score)
+		$ListNode.add_child(node)
 		
+		
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	if response_code != 200:
+		print("ERROR ", body)
+		return
+
+	var json = JSON.parse(body.get_string_from_utf8())
+	makeList(json.result)
---
+++ scripts/SaveResult.gd
@@ -0,0 +1,85 @@
+extends Node2D
+
+var cursorIndex = 0
+var set = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var result = [0, 0, 0, 0, 0, 0]
+var score = 0
+var url = "http://redspirit.ru:5500/api/save"
+var doReturn = false
+
+func _ready():
+	var data = Scores.getData()
+	score = data.score
+	$ScoresValue.text = str(data.score)
+	$NotifLabel.visible = false
+
+func _process(delta):
+	
+	if Input.is_action_just_pressed("ui_right"):
+		cursorIndex += 1
+		if cursorIndex > 5:
+			cursorIndex = 0
+
+	if Input.is_action_just_pressed("ui_left"):
+		cursorIndex -= 1
+		if cursorIndex < 0:
+			cursorIndex = 5
+
+	if Input.is_action_just_pressed("ui_up"):
+		result[cursorIndex] += 1
+		if result[cursorIndex] > 26:
+			result[cursorIndex] = 0
+		drawLatter()
+		
+	if Input.is_action_just_pressed("ui_down"):
+		result[cursorIndex] -= 1
+		if result[cursorIndex] < 0:
+			result[cursorIndex] = 26
+		drawLatter()
+		
+	if Input.is_action_just_pressed("ui_accept") :
+		saveName()
+		
+	$NameNode/ArrowNode/ArrowLabel.rect_position.x = cursorIndex * 48
+	
+func drawLatter():
+	var elem = $NameNode.get_node("lab_" + str(cursorIndex + 1))
+	elem.text = set[result[cursorIndex]]
+	
+func saveName():
+	
+	var name = ""
+	
+	for i in result:
+		if i > 0 :
+			name += set[i]
+	
+	if name.length() < 3:
+		$NotifLabel.text = "3 latters minimum"
+		$NotifLabel.visible = true
+		$Timer.start()
+		doReturn = false
+		return
+	
+	makeRequest(name)
+
+func makeRequest(name):
+	var query = JSON.print({"name":name, "score": score})
+	var headers = ["Content-Type: application/json"]
+	$HTTPRequest.request(url, headers, false, HTTPClient.METHOD_POST, query)
+
+func _on_Timer_timeout():
+	$NotifLabel.visible = false
+	if doReturn:
+		get_tree().change_scene("res://scenes/Leaders.tscn")
+
+
+func _on_HTTPRequest_request_completed(result, response_code, headers, body):
+	if response_code == 200:
+		$NotifLabel.text = "saved"
+		$NotifLabel.visible = true
+		doReturn = true
+	else :
+		$NotifLabel.text = "server error"
+		$NotifLabel.visible = true
+		doReturn = true
