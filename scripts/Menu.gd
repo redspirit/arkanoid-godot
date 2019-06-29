ada13b069bac3ba171274246c73cd9ee5ed7260c
Alexey <redspirit2@ya.ru>
2019-06-30 00:14:22 +0300

    menu

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -8,38 +8,12 @@ func _ready():
 	$highscoreValue.text = str(data.score)
 	$Music.play()
 
-func _process(delta):
-	
-	if Input.is_action_just_pressed("ui_down"):
-		cursorIndex += 1
-		if cursorIndex > 2:
-			cursorIndex = 2
-			return
-		$Select.play()
-	
-	if Input.is_action_just_pressed("ui_up"):
-		cursorIndex -= 1
-		if cursorIndex < 0:
-			cursorIndex = 0
-			return
-		$Select.play()
-	
-	if Input.is_action_just_pressed("ui_accept") :
-		selecItem()
-		
-	$Options/Sprite.position.y = cursorIndex * 48
-	
-func selecItem():
-	
-	if cursorIndex == 0 :
-		get_tree().change_scene("res://scenes/Main.tscn")
-	elif cursorIndex == 1:
-		get_tree().change_scene("res://scenes/Leaders.tscn")
-	elif cursorIndex == 2 :
-		get_tree().quit()
-		
-		
 
-func _on_startLabel_gui_input(event):
-	print(event)
-	$Select.play()
+func _on_TouchScreenButton_pressed():
+	get_tree().change_scene("res://scenes/Main.tscn")
+
+func _on_TouchLeaders_pressed():
+	get_tree().change_scene("res://scenes/Leaders.tscn")
+
+func _on_TouchExit_pressed():
+	get_tree().quit()
